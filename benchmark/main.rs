abed830cc66c4f2e7c904f2da78fb52de4a77c1f
Ming <test@example.com>
2023-08-12 14:23:50 -0700

    Update standard mcts_random_playout_bias

--- benchmark/main.rs
+++ benchmark/main.rs
@@ -165,7 +165,7 @@ fn standard_search_opts(algorithm: Option<SearchAlgorithm>, standard_time_limit_
         mcts_b: None,
         mcts_random_playout_iters: Some(10),
         mcts_random_playout_max_steps: Some(20),
-        mcts_random_playout_bias: Some(10.0),
+        mcts_random_playout_bias: Some(50.0),
         time_limit_ms: standard_time_limit_ms.or(Some(300)),
         tt_size_mb: Some(32),
         search_depth: Some(4),
