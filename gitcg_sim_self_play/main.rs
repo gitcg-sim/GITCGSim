030f52a4de9ceb4a64a0d27d7f7e738bb07406ca
Ming <test@example.com>
2023-09-23 23:53:31 -0700

    Move policy network to main crate

--- gitcg_sim_self_play/main.rs
+++ gitcg_sim_self_play/main.rs
@@ -1,7 +1,6 @@
 use std::{
     fmt::Debug,
-    ops::{ControlFlow, Mul},
-    path::PathBuf,
+    ops::{ControlFlow, Mul}
 };
 
 use dfdx::{optim::Sgd, prelude::*};
@@ -10,7 +9,8 @@ use gitcg_sim::{
     rand::{rngs::SmallRng, thread_rng, Rng, SeedableRng},
     training::{
         as_slice::*,
-        features::{GameStateFeatures, InputFeatures},
+        policy::*,
+        features::*,
     },
 };
 use serde::Serialize;
@@ -242,59 +242,6 @@ fn main_tdl(mut deck: DeckOpts, opts: TDLOpts) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-// const H: usize = 3;
-// type Model = (Linear<N, H>, Sigmoid, Linear<H, K>, Sigmoid);
-pub const N: usize = <GameStateFeatures<f32> as AsSlice<f32>>::LENGTH;
-pub const K: usize = <InputFeatures<f32> as AsSlice<f32>>::LENGTH;
-pub type Model = (Linear<N, K>, Sigmoid);
-
-pub struct PolicyNetwork {
-    pub dev: Cpu,
-    pub model: <(Linear<N, K>, Sigmoid) as dfdx::nn::BuildOnDevice<Cpu, f32>>::Built,
-}
-
-impl PolicyNetwork {
-    pub fn new() -> Self {
-        let dev = Cpu::default();
-        Self {
-            dev: dev.clone(),
-            model: dev.build_module::<Model, f32>(),
-        }
-    }
-
-    pub fn from_npz(path: &PathBuf) -> Result<Self, String> {
-        let mut model = Self::new();
-        model.load_from_npz(path)?;
-        Ok(model)
-    }
-
-    pub fn alloc_grads(&self) -> Gradients<f32, Cpu> {
-        self.model.alloc_grads()
-    }
-
-    pub fn alloc_x<const BATCH_SIZE: usize>(&self) -> Tensor<Rank2<BATCH_SIZE, N>, f32, Cpu> {
-        self.dev.zeros()
-    }
-
-    pub fn alloc_y<const BATCH_SIZE: usize>(&self) -> Tensor<Rank2<BATCH_SIZE, K>, f32, Cpu> {
-        self.dev.zeros()
-    }
-
-    pub fn load_from_npz(&mut self, path: &PathBuf) -> Result<(), String> {
-        self.model.load(path).map_err(|e| e.to_string())
-    }
-
-    pub fn save_npz(&mut self, path: &PathBuf) -> Result<(), String> {
-        self.model.save(path).map_err(|e| e.to_string())
-    }
-}
-
-impl Default for PolicyNetwork {
-    fn default() -> Self {
-        Self::new()
-    }
-}
-
 fn generate_batch<S: NondetState>(
     initial: &GameStateWrapper<S>,
     data_points: &mut Vec<(GameStateFeatures<f32>, InputFeatures<f32>, u8)>,
@@ -346,6 +293,8 @@ fn main_policy(deck: DeckOpts, opts: PolicyOpts) -> Result<(), std::io::Error> {
         parallel: true,
     };
 
+    let make_search = move || MCTS::new(config);
+
     let mut model: PolicyNetwork = PolicyNetwork::default();
     if let Some(npz_path) = &opts.load_npz {
         if let Err(e) = model.load_from_npz(npz_path) {
@@ -367,7 +316,7 @@ fn main_policy(deck: DeckOpts, opts: PolicyOpts) -> Result<(), std::io::Error> {
     );
     let mut data_points: Vec<(GameStateFeatures<f32>, InputFeatures<f32>, u8)> = vec![];
     for iter in 0..250 {
-        let mut searches = ByPlayer::new(MCTS::new(config), MCTS::new(config));
+        let mut searches = ByPlayer::new(make_search(), make_search());
         let initial = deck.get_standard_game(Some(SmallRng::from_seed(seed_gen.gen())))?;
         let batch_generated = generate_batch(
             &initial,
--- src/training/policy.rs
+++ src/training/policy.rs
@@ -1,1 +1,61 @@
+use std::path::PathBuf;
+use dfdx::prelude::*;
 
+use crate::training::{
+    as_slice::*,
+    features::{GameStateFeatures, InputFeatures},
+};
+
+// const H: usize = 3;
+// type Model = (Linear<N, H>, Sigmoid, Linear<H, K>, Sigmoid);
+pub const N: usize = <GameStateFeatures<f32> as AsSlice<f32>>::LENGTH;
+pub const K: usize = <InputFeatures<f32> as AsSlice<f32>>::LENGTH;
+pub type Model = (Linear<N, K>, Sigmoid);
+
+#[derive(Debug, Clone)]
+pub struct PolicyNetwork {
+    pub dev: Cpu,
+    pub model: <(Linear<N, K>, Sigmoid) as dfdx::nn::BuildOnDevice<Cpu, f32>>::Built,
+}
+
+impl PolicyNetwork {
+    pub fn new() -> Self {
+        let dev = Cpu::default();
+        Self {
+            dev: dev.clone(),
+            model: dev.build_module::<Model, f32>(),
+        }
+    }
+
+    pub fn from_npz(path: &PathBuf) -> Result<Self, String> {
+        let mut model = Self::new();
+        model.load_from_npz(path)?;
+        Ok(model)
+    }
+
+    pub fn alloc_grads(&self) -> Gradients<f32, Cpu> {
+        self.model.alloc_grads()
+    }
+
+    pub fn alloc_x<const BATCH_SIZE: usize>(&self) -> Tensor<Rank2<BATCH_SIZE, N>, f32, Cpu> {
+        self.dev.zeros()
+    }
+
+    pub fn alloc_y<const BATCH_SIZE: usize>(&self) -> Tensor<Rank2<BATCH_SIZE, K>, f32, Cpu> {
+        self.dev.zeros()
+    }
+
+    pub fn load_from_npz(&mut self, path: &PathBuf) -> Result<(), String> {
+        self.model.load(path).map_err(|e| e.to_string())
+    }
+
+    pub fn save_npz(&mut self, path: &PathBuf) -> Result<(), String> {
+        self.model.save(path).map_err(|e| e.to_string())
+    }
+}
+
+impl Default for PolicyNetwork {
+    fn default() -> Self {
+        Self::new()
+    }
+}
