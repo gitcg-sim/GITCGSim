b4f4129b5697c4a0e8985f8e37e8a6a7df25550f
Ming <test@example.com>
2023-09-23 23:27:55 -0700

    Refactor policy network code

--- gitcg_sim_self_play/main.rs
+++ gitcg_sim_self_play/main.rs
@@ -1,6 +1,10 @@
-use std::{fmt::Debug, ops::ControlFlow};
+use std::{
+    fmt::Debug,
+    ops::{ControlFlow, Mul},
+    path::PathBuf,
+};
 
-use dfdx::{optim::Sgd, prelude::*, tensor::*};
+use dfdx::{optim::Sgd, prelude::*};
 use gitcg_sim::{
     mcts::{MCTSConfig, MCTS},
     rand::{rngs::SmallRng, thread_rng, Rng, SeedableRng},
@@ -64,6 +68,12 @@ pub struct PolicyOpts {
     pub mcts_time_limit_ms: f32,
     #[structopt(default_value = "4", long = "--min-depth")]
     pub mcts_min_depth: u8,
+    #[structopt(long = "--l1-regularization")]
+    pub l1_regularization: Option<f32>,
+    #[structopt(long = "--save-npz")]
+    pub save_npz: Option<std::path::PathBuf>,
+    #[structopt(long = "--load-npz")]
+    pub load_npz: Option<std::path::PathBuf>,
 }
 
 #[derive(Debug, StructOpt, Clone)]
@@ -86,7 +96,7 @@ pub enum SelfPlayOpts {
 }
 
 fn run_playout<
-    T: GameTreeSearch<GameStateWrapper<S>> + GetSelfPlayModel,
+    T: GameTreeSearch<GameStateWrapper<S>>,
     S: NondetState,
     C: FnMut(&ByPlayer<T>, &GameStateWrapper<S>, Input) -> ControlFlow<()>,
 >(
@@ -234,10 +244,91 @@ fn main_tdl(mut deck: DeckOpts, opts: TDLOpts) -> Result<(), std::io::Error> {
     Ok(())
 }
 
-const H: usize = 8;
-const N: usize = <GameStateFeatures<f32> as AsSlice<f32>>::LENGTH;
-const K: usize = <InputFeatures<f32> as AsSlice<f32>>::LENGTH;
-type Model = (Linear<N, H>, Sigmoid, Linear<H, K>, Sigmoid);
+// const H: usize = 3;
+// type Model = (Linear<N, H>, Sigmoid, Linear<H, K>, Sigmoid);
+pub const N: usize = <GameStateFeatures<f32> as AsSlice<f32>>::LENGTH;
+pub const K: usize = <InputFeatures<f32> as AsSlice<f32>>::LENGTH;
+pub type Model = (Linear<N, K>, Sigmoid);
+
+pub struct PolicyNetwork {
+    pub dev: Cpu,
+    pub model: <(Linear<N, K>, Sigmoid) as dfdx::nn::BuildOnDevice<Cpu, f32>>::Built,
+}
+
+impl PolicyNetwork {
+    pub fn new() -> Self {
+        let dev = Cpu::default();
+        Self {
+            dev: dev.clone(),
+            model: dev.build_module::<Model, f32>(),
+        }
+    }
+
+    pub fn from_npz(path: &PathBuf) -> Result<Self, String> {
+        let mut model = Self::new();
+        model.load_from_npz(path)?;
+        Ok(model)
+    }
+
+    pub fn alloc_grads(&self) -> Gradients<f32, Cpu> {
+        self.model.alloc_grads()
+    }
+
+    pub fn alloc_x<const BATCH_SIZE: usize>(&self) -> Tensor<Rank2<BATCH_SIZE, N>, f32, Cpu> {
+        self.dev.zeros()
+    }
+
+    pub fn alloc_y<const BATCH_SIZE: usize>(&self) -> Tensor<Rank2<BATCH_SIZE, K>, f32, Cpu> {
+        self.dev.zeros()
+    }
+
+    pub fn load_from_npz(&mut self, path: &PathBuf) -> Result<(), String> {
+        self.model.load(path).map_err(|e| e.to_string())
+    }
+
+    pub fn save_npz(&mut self, path: &PathBuf) -> Result<(), String> {
+        self.model.save(path).map_err(|e| e.to_string())
+    }
+}
+
+impl Default for PolicyNetwork {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+fn generate_batch<S: NondetState>(
+    initial: &GameStateWrapper<S>,
+    data_points: &mut Vec<(GameStateFeatures<f32>, InputFeatures<f32>, u8)>,
+    searches: &mut ByPlayer<MCTS<GameStateWrapper<S>>>,
+    mcts_min_depth: u8,
+    batch_size: usize,
+) -> bool {
+    let mut batch_generated = false;
+    while data_points.len() < batch_size {
+        run_playout(initial, searches, 300, |searches, _game_state, _input| {
+            let player_id = PlayerId::PlayerFirst;
+            let mcts = &searches[player_id];
+            let mut vec = vec![];
+            mcts.get_self_play_data_points(player_id, mcts_min_depth, &mut vec);
+            for (gs, input, depth) in vec {
+                if input.player() != Some(PlayerId::PlayerFirst) {
+                    continue;
+                }
+                let features = gs.features();
+                let input_features = input.features(1f32);
+                if data_points.len() >= batch_size {
+                    return ControlFlow::Break(());
+                }
+                data_points.push((features, input_features, depth));
+            }
+            ControlFlow::Continue(())
+        })
+        .unwrap();
+        batch_generated = true;
+    }
+    batch_generated
+}
 
 fn main_policy(deck: DeckOpts, opts: PolicyOpts) -> Result<(), std::io::Error> {
     let mut seed_gen = thread_rng();
@@ -257,92 +348,86 @@ fn main_policy(deck: DeckOpts, opts: PolicyOpts) -> Result<(), std::io::Error> {
         parallel: true,
     };
 
-    let dev: Cpu = Default::default();
-    let mut model = dev.build_module::<Model, f32>();
+    let mut model: PolicyNetwork = PolicyNetwork::default();
+    if let Some(npz_path) = &opts.load_npz {
+        if let Err(e) = model.load_from_npz(npz_path) {
+            println!(
+                "{}",
+                json!({
+                    "load_error": format!("{e}")
+                })
+            );
+        };
+    }
     let mut grads = model.alloc_grads();
-    let mut opt = Sgd::new(&model, Default::default());
+    let mut opt = Sgd::new(
+        &model.model,
+        SgdConfig {
+            lr: 1e-1,
+            ..Default::default()
+        },
+    );
+    let mut data_points: Vec<(GameStateFeatures<f32>, InputFeatures<f32>, u8)> = vec![];
     for iter in 0..250 {
         let mut searches = ByPlayer::new(MCTS::new(config), MCTS::new(config));
         let initial = deck.get_standard_game(Some(SmallRng::from_seed(seed_gen.gen())))?;
-        let mut data_points: Vec<(GameStateFeatures<f32>, InputFeatures<f32>, u8)> = vec![];
-        while data_points.len() < BATCH_SIZE {
-            run_playout(&initial, &mut searches, 300, |searches, _game_state, _input| {
-                let player_id = PlayerId::PlayerFirst;
-                let mcts = &searches[player_id];
-                let mut vec = vec![];
-                mcts.get_self_play_data_points(player_id, opts.mcts_min_depth, &mut vec);
-                for (gs, input, depth) in vec {
-                    if input.player() != Some(PlayerId::PlayerFirst) {
-                        continue;
-                    }
-                    let features = gs.features();
-                    let input_features = input.features(1f32);
-                    #[cfg(any())]
-                    println!(
-                        "{{\"depth\": {depth}, \"game_state\": {}, \"input\": {}}}",
-                        serde_json::to_string(&features).unwrap(),
-                        serde_json::to_string(&input_features).unwrap()
-                    );
-                    if data_points.len() >= BATCH_SIZE {
-                        return ControlFlow::Break(());
-                    }
-                    data_points.push((features, input_features, depth));
-                }
-                ControlFlow::Continue(())
-            })
-            .unwrap();
+        let batch_generated = generate_batch(
+            &initial,
+            &mut data_points,
+            &mut searches,
+            opts.mcts_min_depth,
+            BATCH_SIZE,
+        );
+        let batch = (0..BATCH_SIZE).map(|_| data_points.pop().unwrap()).collect::<Vec<_>>();
+        let (inputs, outputs): (Vec<_>, Vec<_>) = batch
+            .iter()
+            .take(BATCH_SIZE)
+            .map(|(x, y, _)| (x.as_slice(), y.as_slice()))
+            .unzip();
+        let (inputs, outputs) = (inputs.concat(), outputs.concat());
+
+        fn t_batched<const L: usize>(inputs: Vec<f32>) -> Array1<f32> {
+            Array2::from_shape_vec((L, BATCH_SIZE), inputs)
+                .unwrap()
+                .t()
+                .to_owned()
+                .into_shape((BATCH_SIZE * L,))
+                .unwrap()
         }
 
-        // let rows = data_points.len();
-        let input_data = {
-            let inputs = data_points
-                .iter()
-                .take(BATCH_SIZE)
-                .flat_map(|(x, _, _)| x.as_slice())
-                .collect::<Vec<f32>>();
-            Array2::from_shape_vec((N, BATCH_SIZE), inputs).unwrap().t().to_owned()
-        };
-        let output_data = {
-            let inputs = data_points
-                .iter()
-                .take(BATCH_SIZE)
-                .flat_map(|(_, y, _)| y.as_slice())
-                .collect::<Vec<f32>>();
-            Array2::from_shape_vec((K, BATCH_SIZE), inputs).unwrap().t().to_owned()
-        };
+        let input_data = t_batched::<N>(inputs);
+        let output_data = t_batched::<K>(outputs);
 
-        let mut x: Tensor<Rank2<BATCH_SIZE, N>, f32, _> = dev.zeros();
-        x.copy_from(input_data.into_shape((BATCH_SIZE * N,)).unwrap().as_slice().unwrap());
-        let y = model.forward_mut(x.traced(grads));
+        let dev = &model.dev;
+        let x = {
+            let mut x = model.alloc_x::<BATCH_SIZE>();
+            x.copy_from(input_data.as_slice().unwrap());
+            x
+        };
+        let y = model.model.forward_mut(x.traced(grads));
         let mut y0: Tensor<Rank2<BATCH_SIZE, K>, f32, _> = dev.zeros();
-        y0.copy_from(output_data.into_shape((BATCH_SIZE * K,)).unwrap().as_slice().unwrap());
-        let loss = (y - y0).square().mean();
+        y0.copy_from(output_data.as_slice().unwrap());
+        let error = (y - y0).square().mean();
+        let loss = if let Some(l1) = opts.l1_regularization {
+            error + (/* model.0.bias.clone().abs().sum() + */model.model.0.weight.clone().abs().sum()).mul(l1)
+        } else {
+            error
+        };
         let loss_value = loss.array();
         grads = loss.backward();
-        opt.update(&mut model, &grads).unwrap();
+        opt.update(&mut model.model, &grads).unwrap();
         println!(
             "{}",
             json!({
                 "loss": loss_value,
-                "iter": iter
+                "iter": iter,
+                "batch_generated": batch_generated,
             })
         );
-        #[cfg(any())]
-        {
-            let policy_matrix_struct = {
-                let flattened: [f32; N * K] = unsafe { std::mem::transmute(model.0.weight.array()) };
-                <InputFeatures<GameStateFeatures<f32>> as AsSlice>::from_slice(flattened)
-            };
-            let policy_bias_struct = <InputFeatures<f32> as AsSlice>::from_slice(model.0.bias.array());
-            let log_entry = json!({
-                "policy": {
-                    "matrix": policy_matrix_struct,
-                    "bias": policy_bias_struct
-                },
-                "iter": iter,
-                "loss": loss_value
-            });
-            println!("{}", log_entry);
+        if iter % 5 == 0 {
+            if let Some(npz_path) = &opts.save_npz {
+                model.save_npz(npz_path).unwrap();
+            }
         }
     }
     Ok(())
