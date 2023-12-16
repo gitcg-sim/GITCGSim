dbbe59385165bec32092c55cb952df03761148f5
Ming <test@example.com>
2023-12-16 13:39:33 -0800

    Remove unused methods for capped_list

--- src/data_structures/capped_list.rs
+++ src/data_structures/capped_list.rs
@@ -15,28 +15,6 @@ pub enum CappedLengthList8<T> {
     L88(T, T, T, T, T, T, T, T),
 }
 
-impl<T: PartialEq> CappedLengthList8<T> {
-    pub fn contains(&self, e: &T) -> bool {
-        match self {
-            CappedLengthList8::L08 => false,
-            CappedLengthList8::L18(v0) => v0 == e,
-            CappedLengthList8::L28(v0, v1) => v0 == e || v1 == e,
-            CappedLengthList8::L38(v0, v1, v2) => v0 == e || v1 == e || v2 == e,
-            CappedLengthList8::L48(v0, v1, v2, v3) => v0 == e || v1 == e || v2 == e || v3 == e,
-            CappedLengthList8::L58(v0, v1, v2, v3, v4) => v0 == e || v1 == e || v2 == e || v3 == e || v4 == e,
-            CappedLengthList8::L68(v0, v1, v2, v3, v4, v5) => {
-                v0 == e || v1 == e || v2 == e || v3 == e || v4 == e || v5 == e
-            }
-            CappedLengthList8::L78(v0, v1, v2, v3, v4, v5, v6) => {
-                v0 == e || v1 == e || v2 == e || v3 == e || v4 == e || v5 == e || v6 == e
-            }
-            CappedLengthList8::L88(v0, v1, v2, v3, v4, v5, v6, v7) => {
-                v0 == e || v1 == e || v2 == e || v3 == e || v4 == e || v5 == e || v6 == e || v7 == e
-            }
-        }
-    }
-}
-
 impl<T: Copy> CappedLengthList8<T> {
     pub fn to_vec(self) -> smallvec::SmallVec<[T; 8]> {
         match self {
@@ -63,22 +41,6 @@ impl<T: Copy + EnumSetTypeWithRepr> CappedLengthList8<T> {
 
 impl<T: Copy, A: smallvec::Array<Item = T>> From<smallvec::SmallVec<A>> for CappedLengthList8<T> {
     fn from(v: smallvec::SmallVec<A>) -> Self {
-        match v.len() {
-            0 => Self::L08,
-            1 => Self::L18(v[0]),
-            2 => Self::L28(v[0], v[1]),
-            3 => Self::L38(v[0], v[1], v[2]),
-            4 => Self::L48(v[0], v[1], v[2], v[3]),
-            5 => Self::L58(v[0], v[1], v[2], v[3], v[4]),
-            6 => Self::L68(v[0], v[1], v[2], v[3], v[4], v[5]),
-            7 => Self::L78(v[0], v[1], v[2], v[3], v[4], v[5], v[6]),
-            _ => Self::L88(v[0], v[1], v[2], v[3], v[4], v[5], v[6], v[7]),
-        }
-    }
-}
-
-impl<T: Copy> From<Vec<T>> for CappedLengthList8<T> {
-    fn from(v: Vec<T>) -> Self {
         match v.len() {
             0 => Self::L08,
             1 => Self::L18(v[0]),
@@ -125,92 +87,6 @@ impl<T> CappedLengthList8<T> {
             CappedLengthList8::L88(v0, v1, v2, v3, v4, v5, v6, v7) => {
                 f(f(f(f(f(f(f(f(init, v0), v1), v2), v3), v4), v5), v6), v7)
             }
-        }
-    }
-
-    pub fn map<R>(&self, f: fn(&T) -> R) -> CappedLengthList8<R> {
-        match self {
-            CappedLengthList8::L08 => CappedLengthList8::L08,
-            CappedLengthList8::L18(v0) => CappedLengthList8::L18(f(v0)),
-            CappedLengthList8::L28(v0, v1) => CappedLengthList8::L28(f(v0), f(v1)),
-            CappedLengthList8::L38(v0, v1, v2) => CappedLengthList8::L38(f(v0), f(v1), f(v2)),
-            CappedLengthList8::L48(v0, v1, v2, v3) => CappedLengthList8::L48(f(v0), f(v1), f(v2), f(v3)),
-            CappedLengthList8::L58(v0, v1, v2, v3, v4) => CappedLengthList8::L58(f(v0), f(v1), f(v2), f(v3), f(v4)),
-            CappedLengthList8::L68(v0, v1, v2, v3, v4, v5) => {
-                CappedLengthList8::L68(f(v0), f(v1), f(v2), f(v3), f(v4), f(v5))
-            }
-            CappedLengthList8::L78(v0, v1, v2, v3, v4, v5, v6) => {
-                CappedLengthList8::L78(f(v0), f(v1), f(v2), f(v3), f(v4), f(v5), f(v6))
-            }
-            CappedLengthList8::L88(v0, v1, v2, v3, v4, v5, v6, v7) => {
-                CappedLengthList8::L88(f(v0), f(v1), f(v2), f(v3), f(v4), f(v5), f(v6), f(v7))
-            }
-        }
-    }
-
-    pub fn index(&self, idx: u8) -> Option<&T> {
-        match self {
-            CappedLengthList8::<T>::L08 => None,
-            CappedLengthList8::L18(v0) => match idx {
-                0 => Some(v0),
-                _ => None,
-            },
-            CappedLengthList8::L28(v0, v1) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                _ => None,
-            },
-            CappedLengthList8::L38(v0, v1, v2) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                2 => Some(v2),
-                _ => None,
-            },
-            CappedLengthList8::L48(v0, v1, v2, v3) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                2 => Some(v2),
-                3 => Some(v3),
-                _ => None,
-            },
-            CappedLengthList8::L58(v0, v1, v2, v3, v4) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                2 => Some(v2),
-                3 => Some(v3),
-                4 => Some(v4),
-                _ => None,
-            },
-            CappedLengthList8::L68(v0, v1, v2, v3, v4, v5) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                2 => Some(v2),
-                3 => Some(v3),
-                4 => Some(v4),
-                5 => Some(v5),
-                _ => None,
-            },
-            CappedLengthList8::L78(v0, v1, v2, v3, v4, v5, v6) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                2 => Some(v2),
-                3 => Some(v3),
-                4 => Some(v4),
-                5 => Some(v5),
-                6 => Some(v6),
-                _ => None,
-            },
-            CappedLengthList8::L88(v0, v1, v2, v3, v4, v5, v6, v7) => match idx {
-                0 => Some(v0),
-                1 => Some(v1),
-                2 => Some(v2),
-                3 => Some(v3),
-                4 => Some(v4),
-                5 => Some(v5),
-                6 => Some(v6),
-                7 => Some(v7),
-                _ => None,
-            },
         }
     }
 }
